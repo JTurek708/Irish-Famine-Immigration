fe30b7bcfaea217d047dc9a572ff7eb81254b9bb
Jack Turek <jackturek@jacks-mbp.lan>
2025-04-12 22:10:15 -0500

    Code to process FIPAS records

---
+++ code/process_fipas.py
@@ -0,0 +1,7 @@
+from parse_fipas_file import parse_famine_irish_records
+
+if __name__ == "__main__":
+    # Replace with the actual path to your FIPAS file
+    file_path = "path/to/your/FIPAS.txt"
+
+df = parse_famine_irish_records('/Users/jackturek/Documents/Repos/Irish-Famine-Immigration/data/Famine_Irish_Passengers.txt')
